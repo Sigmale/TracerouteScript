1cfbbe25b92352003065f75b2c19747371bef1c4
Jyotiraditya Yadav <95582292+Sigmale@users.noreply.github.com>
2023-08-13 22:55:41 +0530

    Update script.py

--- script.py
+++ script.py
@@ -1,36 +1,30 @@
 import subprocess
 import re
-
-def traceroute_ping(destination, max_hops=64):
+def traceroute(destination, max_hops=64):
     prev_ip = None
     hop_count = 1
     while hop_count <= max_hops:
         ping_command = ["ping", "-c", "3", "-m", str(hop_count), destination]
         ping_output = subprocess.run(ping_command, capture_output=True, text=True).stdout
 
-        match_ip = re.search(r'bytes from (\d+\.\d+\.\d+\.\d+)', ping_output)
-        if match_ip:
-            ip_addr = match_ip.group(1)
+        ip_match = re.search(r'bytes from (\d+\.\d+\.\d+\.\d+)', ping_output)
+        if ip_match:
+            ip_address = ip_match.group(1)
 
-            if ip_addr == prev_ip:
+            if ip_address == prev_ip:
                 break
-
-            p_command = ["ping", "-c", "3", ip_addr]
-            p_output = subprocess.run(p_command, capture_output=True, text=True).stdout
-
-            match_time = re.search(r'time=(\d+\.\d+) ms', p_output)
-            response_time = match_time.group(1) if match_time else "***"
-
-            print(f"Hop: {hop_count}, IP: {ip_addr}, Time: {response_time}")
-            prev_ip = ip_addr
+            ping_to_ip_command = ["ping", "-c", "3", ip_address]
+            ping_to_ip_output = subprocess.run(ping_to_ip_command, capture_output=True, text=True).stdout
+            time_match = re.search(r'time=(\d+\.\d+) ms', ping_to_ip_output)
+            response_time = time_match.group(1) if time_match else "**"
+            print(f"Hop: {hop_count}, IP: {ip_address}, Time: {response_time}")
+            prev_ip = ip_address
         else:
-            print(f"Hop: {hop_count}, IP: *** Time: ***")
+            print(f"Hop: {hop_count}, IP: **, Time: **")
 
         hop_count += 1
-
 def main():
     destination = input("Enter the destination address: ")
-    traceroute_ping(destination)
-
+    traceroute(destination)
 if __name__ == "__main__":
     main()
